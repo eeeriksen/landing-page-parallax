a8bb84e236994643832fffca9cee8c52972a0261
Eriksen <eriksen.lezama@gmail.com>
2018-02-13 16:09:31 +0100

    Agregada mejor referencia para parallax del título grande

--- js/start.js
+++ js/start.js
@@ -5,15 +5,15 @@ window.addEventListener('scroll', function(){
 		"translateY(" + y * 0.35 + "px)";
 	//capturo el top del ultimo elemento de la seccion anterior al paralax que viene
 	//y traslado el titulo si es mayor la posicion del scroll
-	var refKuk = document.querySelector('p.second-ro').offsetTop;
-	if (y > refKuk) {
+	var refKuk = document.getElementById('kukenan').offsetTop;
+	if (y > refKuk - (window.innerHeight/1.2)) {
 		document.getElementById('kuti').style.transform =
 			"translateY(" + (y - refKuk) * 0.35 + "px)";
 	}
 	//capturo el top del ultimo elemento de la seccion anterior al paralax que viene
 	//y traslado el titulo si es mayor la posicion del scroll
-	var refSal = document.querySelector('p.second-kuk').offsetTop;
-	if (y > refSal) {
+	var refSal = document.getElementById('salto-angel').offsetTop;
+	if (y > refSal - (window.innerHeight)) {
 		document.getElementById('sati').style.transform =
 			"translateY(" + (y - refSal) * 0.35 + "px)";
 	}
